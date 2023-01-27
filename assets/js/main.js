201974a50e72dc77d7e7597d71b715372b5966b8
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-01-27 17:56:11 +0000

    Add conditional to check browser compatibility
    Utilised if block to first check whether the browser supports the geolocation web API.
    
    Pushed the user current coordinates to an object for easier manipulation later

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 /** Get user's location */
-let userCurrentLocation = [];
-navigator.geolocation.getCurrentPosition(
-  (position) => {
-    const { latitude, longitude } = position.coords;
-    userCurrentLocation.push(latitude, longitude);
-    console.log(`Latitude: ${latitude}`);
-    console.log(`Longitude: ${longitude}`);
-  },
-  (err) => console.log(err)
-);
-console.log(userCurrentLocation);
+let userLocation = {};
+if (window.navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      userLocation.latitude = latitude;
+      userLocation.longitude = longitude;
+    },
+    (err) => console.log(err)
+  );
+}
+console.log(userLocation);
