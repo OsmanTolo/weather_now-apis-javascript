a008089de969675237cdcb7d670cbbf2233d06ff
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-01-27 20:00:17 +0000

    Add event listener on the search button
    The event listener triggers the api query using two fetch statement. The first returns generic info about the city, and the second will return the weather info for that specific city.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+let searchInputEl = document.querySelector("#search-input");
+let searchBtnEl = document.querySelector("#search-button");
+
+let apiKey = "40640050a45cbd8cf8d35ada1e14fee3";
+
 /** Get user's location */
 let userLocation = {};
 if (window.navigator.geolocation) {
@@ -10,4 +15,38 @@ if (window.navigator.geolocation) {
     (err) => console.log(err)
   );
 }
-console.log(userLocation);
+// console.log(userLocation);
+
+searchBtnEl.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let searchCity = searchInputEl.value;
+
+  let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${apiKey}`;
+
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then(function (cityFound) {
+      let city = cityFound[0];
+      console.log(city);
+      console.log(`City Name: ${city.name}`);
+
+      let cityDataQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`;
+
+      fetch(cityDataQueryURL)
+        .then((response) => response.json())
+        .then((cityData) => {
+          let cityWeatherInfo = cityData.list;
+          console.log(cityWeatherInfo);
+
+          cityWeatherInfo.forEach((forcast, i) => {});
+        });
+    });
+});
+
+/** Geolocation URL
+ * https://api.openweathermap.org/geo/1.0/direct?q=bristol&limit=5&appid=40640050a45cbd8cf8d35ada1e14fee3
+ */
+/** Bristol forecastURL
+ * https://api.openweathermap.org/data/2.5/forecast?lat=51.4538022&lon=-2.5972985&appid=40640050a45cbd8cf8d35ada1e14fee3
+ */
