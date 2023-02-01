a722aabbd97c9da24966cc2948b638b42081cdff
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-02-01 17:57:36 +0000

    Add readme content and refactored code to use functions for api query

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# weather_now-apis-javascript
-Another weather forecast dashboard for the web. 
+<br />
+<div align="center">
+  <h1 align="center">WeatherNow or Later</h1>
+  <p align="center">
+    A simple web based application that allows users to see the weather forecast for any city.
+    <br />
+    <a href="https://osmantolo.github.io/weather_now-apis-javascript/">View Demo</a>
+  </p>
+</div>
+
+## About The Project
+
+![Screenshot of the webpage](./assets/images/weather_now.png)
+
+This repo presents a weather forecast application that allows a user to live weather information and view the five day weather forecast for any city. The app will run in the browser and feature dynamically updated HTML, CSS, jQuery and vanilla JavaScript.
+
+The user is presented with an aside section that contain user a user input field for typing the name of any city. Upon searching for a city, the current weather conditions for that city is presented from a query to [OpenWeather API](https://openweathermap.org/api), along with the current date using [Moment.js](https://momentjs.com/) library. The user is further presented with a five day forecast for the searched location. The user can also access their previous searched cities.
+
+The repo serves as my solution to the Module 8 - Weather Dashboard Challenge of Trilogy Skills Front-End Development Boot Camp. This app wil run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript. A clean responsive and accessible user interface is presented for multiple screen sizes.
+
+The project provide evidence of my ability to fetch data from a third party API, and analyse, manipulate, and present the data using JavaScript's Web APIs, and jQuery. The result is a user friendly web app that can be accessed by anyone with an internet connection.
+
+## Usage
+
+Visit this github page [link](https://github.com/OsmanTolo/weather_now-apis-javascript) to access the website. The app is web page based, so the user can interface with the controls just by clicking UI elements on the page.
+
+## License
+
+Distributed under the MIT License.
+
+## Contact
+
+Osman Dumbuya - [@OsmanTolo\_](https://twitter.com/OsmanTolo_)
+
+Project Link: [https://github.com/OsmanTolo/weather_now-apis-javascript](https://github.com/OsmanTolo/weather_now-apis-javascript)
+
+## Acknowledgments
+
+- [Skills for Life](https://skillsforlife.campaign.gov.uk/courses/skills-bootcamps/)
+- [Othneil Drew's Best-README-Template](https://github.com/othneildrew/Best-README-Template)
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ let todayTempEl = document.querySelector("#today-temp");
 let todayWindEl = document.querySelector("#today-wind");
 let todayHumidityEl = document.querySelector("#today-humidity");
 let forecastContainerEl = document.querySelector("#forecast");
+let historyContainerEl = document.querySelector("#history");
 
 let apiKey = "40640050a45cbd8cf8d35ada1e14fee3";
 
@@ -22,23 +23,16 @@ if (window.navigator.geolocation) {
     (err) => console.log(err)
   );
 }
-// console.log(userLocation);
 
-searchBtnEl.addEventListener("click", function (e) {
-  e.preventDefault();
+let searchCity = searchInputEl.value;
 
-  let searchCity = searchInputEl.value;
-
+function getWeatherInfo(searchCity) {
   let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${apiKey}`;
 
   fetch(queryURL)
     .then((response) => response.json())
     .then(function (cityFound) {
       let city = cityFound[0];
-      // console.log(city);
-      // console.log(`City Name: ${city.name}`);
-
-      // Display today's data for searched location
 
       let cityDataQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${apiKey}`;
 
@@ -46,12 +40,9 @@ searchBtnEl.addEventListener("click", function (e) {
         .then((response) => response.json())
         .then((cityData) => {
           let cityWeatherInfo = cityData.list;
-          // console.log(cityWeatherInfo);
-          // console.log(cityData.city.name);
 
           // Display today's data for searched location
           todayDateEl.textContent = displayDate(cityWeatherInfo[0].dt);
-
           searchLocationEl.textContent = cityData.city.name;
           todayTempEl.textContent = cityWeatherInfo[0].main.temp;
           todayHumidityEl.textContent = cityWeatherInfo[0].main.humidity;
@@ -92,28 +83,21 @@ searchBtnEl.addEventListener("click", function (e) {
               </div>
             </div>
             `;
-            // <div class="col-sm">
-            //   <div class="forecast-card">
-            //     <h2 class="card-title mt-1">${moment(forecast.dt, "X").format(
-            //       "dddd"
-            //     )}</h2>
-            //     <img src="http://openweathermap.org/img/wn/${
-            //       forecast.weather[0].icon
-            //     }@2x.png">
-            //     <p class="card-text">Temp: <span id="temp">${
-            //       forecast.main.temp
-            //     }</span></p>
-            //     <p class="card-text">Wind: <span id="wind">${
-            //       forecast.wind.speed
-            //     }</span></p>
-            //     <p class="card-text">
-            //       Humidity: <span id="humidity">${forecast.main.humidity}</span>
-            //     </p>
-            //   </div>
-            // </div>
           });
         });
     });
+}
+
+searchBtnEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  forecastContainerEl.innerHTML = "";
+  getWeatherInfo(searchInputEl.value);
+});
+
+historyContainerEl.addEventListener("click", (e) => {
+  let element = e.target.textContent;
+  getWeatherInfo(element);
+  console.log(element);
 });
 
 function displayDate(unix) {
@@ -126,8 +110,7 @@ function displayDate(unix) {
 /** Bristol forecastURL
  * https://api.openweathermap.org/data/2.5/forecast?lat=51.4538022&lon=-2.5972985&appid=40640050a45cbd8cf8d35ada1e14fee3
  */
-
-/**Openweather Weather condition codes
+/** Openweather Weather condition codes
  * https://openweathermap.org/weather-conditions
  *
  * http://openweathermap.org/img/wn/10d@2x.png
